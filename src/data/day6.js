a5c6875bfef20ef4c22ef44ee375462c9760e3ff
beka serkan <bekaserkan@bekas-MacBook-Pro.local>
2024-05-31 14:45:50 +0600

    change

---
+++ src/data/day6.js
@@ -0,0 +1,429 @@
+import way from "../images/way.jpeg";
+import about_me from "../images/about_me.avif";
+import many from "../images/many.jpeg";
+import then from "../images/then.jpeg";
+import up from "../images/up.jpeg";
+import more from "../images/more.avif";
+import most from "../images/most.jpeg";
+import side from "../images/side.png";
+import down from "../images/down.webp";
+import first from "../images/first.jpeg";
+import now from "../images/now.avif";
+import page from "../images/page.jpeg";
+import found from "../images/found.png";
+import between from "../images/between.jpeg";
+import state from "../images/state.jpeg";
+import story from "../images/story.avif";
+import far from "../images/far.jpeg";
+import sea from "../images/sea.avif";
+import night from "../images/night.jpeg";
+import real from "../images/real.avif";
+import life from "../images/life.jpeg";
+import few from "../images/few.jpeg";
+import north from "../images/north.jpeg";
+import book from "../images/book.jpeg";
+import science from "../images/science.jpeg";
+import eat from "../images/eat.avif";
+import room from "../images/room.jpeg";
+import friend from "../images/friend.avif";
+import horse from "../images/hotrse.jpeg";
+import colour from "../images/colour.webp";
+import face from "../images/fase.avif";
+import wood from "../images/wood.avif";
+import main from "../images/main.png";
+import paper from "../images/paper.webp";
+import group from "../images/group.jpeg";
+import always from "../images/always.jpeg";
+import music from "../images/music.avif";
+import those from "../images/those.jpeg";
+import both from "../images/both.webp";
+import mark from "../images/mark.jpeg";
+import often from "../images/often.jpeg";
+import letter from "../images/letter.jpeg";
+import second from "../images/second.jpeg";
+import enouth from "../images/enouth.webp";
+import usual from "../images/usually.jpeg";
+import young from "../images/young.jpeg";
+import ready from "../images/ready.jpeg";
+import above from "../images/above.jpeg";
+import ever from "../images/ever.jpeg";
+import list from "../images/list.png";
+import though from "../images/though.jpeg";
+import soon from "../images/soon.jpeg";
+import short from "../images/short.jpeg";
+import ship from "../images/ship.avif";
+import area from "../images/area.png";
+import half from "../images/half.jpeg";
+import rock from "../images/rock.jpeg";
+import order from "../images/order.jpeg";
+import fire from "../images/fire.jpeg";
+import south from "../images/south.jpeg";
+import problem from "../images/problem.jpeg";
+import piece from "../images/piece.jpeg";
+import top from "../images/top.jpeg";
+import whole from "../images/whole.jpeg";
+import king from "../images/kind.jpeg";
+import street from "../images/street.avif";
+import inch from "../images/inch.jpeg";
+import surface from "../images/surface.avif";
+import deep from "../images/deep.jpeg";
+import moon from "../images/moon.jpeg";
+import island from "../images/island.webp";
+
+export const data6 = [
+  {
+    word: "Way",
+    image: way,
+    translate: "Путь",
+  },
+  {
+    word: "About me",
+    image: about_me,
+    translate: "Обо мне",
+  },
+  {
+    word: "Many",
+    image: many,
+    translate: "Много",
+  },
+  {
+    word: "Then",
+    image: then,
+    translate: "Затем",
+  },
+  {
+    word: "Up",
+    image: up,
+    translate: "Вверх",
+  },
+  {
+    word: "More",
+    image: more,
+    translate: "Больше",
+  },
+  {
+    word: "Most",
+    image: most,
+    translate: "Самый",
+  },
+  {
+    word: "Side",
+    image: side,
+    translate: "Сторона",
+  },
+  {
+    word: "Down",
+    image: down,
+    translate: "Вниз",
+  },
+  {
+    word: "First",
+    image: first,
+    translate: "Первый",
+  },
+  {
+    word: "Now",
+    image: now,
+    translate: "Сейчас",
+  },
+  {
+    word: "Page",
+    image: page,
+    translate: "Страница",
+  },
+  {
+    word: "Found",
+    image: found,
+    translate: "Найденный",
+  },
+  {
+    word: "Between",
+    image: between,
+    translate: "Между",
+  },
+  {
+    word: "State",
+    image: state,
+    translate: "Государство",
+  },
+  {
+    word: "Story",
+    image: story,
+    translate: "История",
+  },
+  {
+    word: "Far",
+    image: far,
+    translate: "Далеко",
+  },
+  {
+    word: "Sea",
+    image: sea,
+    translate: "Море",
+  },
+  {
+    word: "Night",
+    image: night,
+    translate: "Ночь",
+  },
+  {
+    word: "Real",
+    image: real,
+    translate: "Реальный",
+  },
+  {
+    word: "Life",
+    image: life,
+    translate: "Жизнь",
+  },
+  {
+    word: "Few",
+    image: few,
+    translate: "Несколько",
+  },
+  {
+    word: "North",
+    image: north,
+    translate: "Север",
+  },
+  {
+    word: "Book",
+    image: book,
+    translate: "Книга",
+  },
+  {
+    word: "Science",
+    image: science,
+    translate: "Наука",
+  },
+  {
+    word: "Eat",
+    image: eat,
+    translate: "Есть",
+  },
+  {
+    word: "Room",
+    image: room,
+    translate: "Комната",
+  },
+  {
+    word: "Friend",
+    image: friend,
+    translate: "Друг",
+  },
+  {
+    word: "Horse",
+    image: horse,
+    translate: "Лошадь",
+  },
+  {
+    word: "Colour",
+    image: colour,
+    translate: "Цвет",
+  },
+  {
+    word: "Face",
+    image: face,
+    translate: "Лицо",
+  },
+  {
+    word: "Wood",
+    image: wood,
+    translate: "Дерево",
+  },
+  {
+    word: "Main",
+    image: main,
+    translate: "Главный",
+  },
+  {
+    word: "Paper",
+    image: paper,
+    translate: "Бумага",
+  },
+  {
+    word: "Group",
+    image: group,
+    translate: "Группа",
+  },
+  {
+    word: "Always",
+    image: always,
+    translate: "Навсегда",
+  },
+  {
+    word: "Music",
+    image: music,
+    translate: "Музыка",
+  },
+  {
+    word: "Those",
+    image: those,
+    translate: "Те",
+  },
+  {
+    word: "Both",
+    image: both,
+    translate: "Оба",
+  },
+  {
+    word: "Mark",
+    image: mark,
+    translate: "Отмечать",
+  },
+  {
+    word: "Often",
+    image: often,
+    translate: "Часто",
+  },
+  {
+    word: "Letter",
+    image: letter,
+    translate: "Письмо",
+  },
+  {
+    word: "Second",
+    image: second,
+    translate: "Секунда",
+  },
+  {
+    word: "Enough",
+    image: enouth,
+    translate: "Достаточно",
+  },
+  {
+    word: "Usual",
+    image: usual,
+    translate: "Обычно",
+  },
+  {
+    word: "Young",
+    image: young,
+    translate: "Молодой",
+  },
+  {
+    word: "Ready",
+    image: ready,
+    translate: "Готовый",
+  },
+  {
+    word: "Above",
+    image: above,
+    translate: "Выше",
+  },
+  {
+    word: "Ever",
+    image: ever,
+    translate: "Когда либо",
+  },
+  {
+    word: "List",
+    image: list,
+    translate: "Список",
+  },
+  {
+    word: "Though",
+    image: though,
+    translate: "Хоть",
+  },
+  {
+    word: "Soon",
+    image: soon,
+    translate: "Скоро",
+  },
+  {
+    word: "Short",
+    image: short,
+    translate: "Короткий",
+  },
+  {
+    word: "Ship",
+    image: ship,
+    translate: "Корабль",
+  },
+  {
+    word: "Area",
+    image: area,
+    translate: "Площадь",
+  },
+  {
+    word: "Half",
+    image: half,
+    translate: "Половина",
+  },
+  {
+    word: "Rock",
+    image: rock,
+    translate: "Скала",
+  },
+  {
+    word: "Order",
+    image: order,
+    translate: "Заказ",
+  },
+  {
+    word: "Fire",
+    image: fire,
+    translate: "Огонь",
+  },
+  {
+    word: "South",
+    image: south,
+    translate: "Юг",
+  },
+  {
+    word: "Problem",
+    image: problem,
+    translate: "Проблема",
+  },
+  {
+    word: "Piece",
+    image: piece,
+    translate: "Часть",
+  },
+  {
+    word: "Top",
+    image: top,
+    translate: "Вершина",
+  },
+  {
+    word: "Whole",
+    image: whole,
+    translate: "Целый",
+  },
+  {
+    word: "King",
+    image: king,
+    translate: "Король",
+  },
+  {
+    word: "Street",
+    image: street,
+    translate: "Улица",
+  },
+  {
+    word: "Inch",
+    image: inch,
+    translate: "Дюйм",
+  },
+  {
+    word: "Surface",
+    image: surface,
+    translate: "Поверхность",
+  },
+  {
+    word: "Deep",
+    image: deep,
+    translate: "Глубокий",
+  },
+  {
+    word: "Moon",
+    image: moon,
+    translate: "Луна",
+  },
+  {
+    word: "Island",
+    image: island,
+    translate: "Остров",
+  },
+];
--- src/pages/Lang/Lang.jsx
+++ src/pages/Lang/Lang.jsx
@@ -6,6 +6,7 @@ import { data1 } from "../../data/day1";
 import { data3 } from "../../data/day3";
 import { data4 } from "../../data/day4";
 import { data5 } from "../../data/day5";
+import { data6 } from "../../data/day6";
 
 const allData = {
   day1: data1,
@@ -18,7 +19,9 @@ const allData = {
 function Lang() {
   const [value, setValue] = useState("");
   const [activeTab, setActiveTab] = useState("day2");
-  const [flipped, setFlipped] = useState(Array(allData[activeTab].length).fill(false));
+  const [flipped, setFlipped] = useState(
+    Array(allData[activeTab].length).fill(false)
+  );
 
   const handleFlip = (index) => {
     setFlipped((prevFlipped) =>
@@ -31,7 +34,7 @@ function Lang() {
     setFlipped(Array(allData[tab].length).fill(false));
   };
 
-  const data = allData[activeTab];
+  const data = data6;
 
   const filteredData = data.filter(
     (el) =>
@@ -49,23 +52,38 @@ function Lang() {
           onChange={(e) => setValue(e.target.value)}
         />
       </div>
-      <div className="wrapper">
-        <div className={`tab ${activeTab === "day1" ? "active" : ""}`} onClick={() => handleTabChange("day1")}>
+      {/* <div className="wrapper">
+        <div
+          className={`tab ${activeTab === "day1" ? "active" : ""}`}
+          onClick={() => handleTabChange("day1")}
+        >
           День 1
         </div>
-        <div className={`tab ${activeTab === "day2" ? "active" : ""}`} onClick={() => handleTabChange("day2")}>
+        <div
+          className={`tab ${activeTab === "day2" ? "active" : ""}`}
+          onClick={() => handleTabChange("day2")}
+        >
           День 2
         </div>
-        <div className={`tab ${activeTab === "day3" ? "active" : ""}`} onClick={() => handleTabChange("day3")}>
+        <div
+          className={`tab ${activeTab === "day3" ? "active" : ""}`}
+          onClick={() => handleTabChange("day3")}
+        >
           День 3
         </div>
-        <div className={`tab ${activeTab === "day4" ? "active" : ""}`} onClick={() => handleTabChange("day4")}>
+        <div
+          className={`tab ${activeTab === "day4" ? "active" : ""}`}
+          onClick={() => handleTabChange("day4")}
+        >
           День 4
         </div>
-        <div className={`tab ${activeTab === "day5" ? "active" : ""}`} onClick={() => handleTabChange("day5")}>
+        <div
+          className={`tab ${activeTab === "day5" ? "active" : ""}`}
+          onClick={() => handleTabChange("day5")}
+        >
           День 5
         </div>
-      </div>
+      </div> */}
       <div className="content">
         {filteredData.map((el, index) => (
           <div
